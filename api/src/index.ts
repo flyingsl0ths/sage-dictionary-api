b343b2b4fc41f9ef12f71c8afb065b844c5b6535
flyingsl0ths <randomemail33110@protonmail.com>
2025-07-02 21:23:26 -0700

    Migrate to node.js

---
+++ api/src/index.ts
@@ -0,0 +1,43 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { parseWord } from "./word.js";
+
+const app = new Hono();
+
+app.get("/:word", async (c) => {
+	const word = c.req.param("word");
+
+	const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+	const response = await fetch(`${BASE_URL}${word}`);
+
+	if (response.status !== 200) {
+		return new Response("Not found", { status: 404 });
+	}
+
+	const json = await response.json();
+
+	const info = parseWord(json[0]);
+
+	if (!info) {
+		return new Response("Not enough info", { status: 404 });
+	}
+
+	return c.json(info);
+});
+
+const server = serve(app);
+
+process.on("SIGINT", () => {
+	server.close();
+	process.exit(0);
+});
+process.on("SIGTERM", () => {
+	server.close((err) => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		process.exit(0);
+	});
+});
