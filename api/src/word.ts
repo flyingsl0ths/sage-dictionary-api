6782d4a3dc8754d730d1b7a3f92d9c29daf1f32d
flyingsl0ths <randomemail33110@protonmail.com>
2025-07-14 02:12:37 -0700

    Fix typo

--- api/src/word.ts
+++ api/src/word.ts
@@ -1,60 +1,60 @@
 export type Word = {
-	readonly word: string;
-	readonly pronounciation: string;
-	readonly definition: string;
-	readonly synonyms: string[];
-	readonly antonyms: string[];
-	readonly sentences: string[];
+  readonly word: string;
+  readonly phonetics: string;
+  readonly definition: string;
+  readonly synonyms: string[];
+  readonly antonyms: string[];
+  readonly sentences: string[];
 };
 
 // biome-ignore lint: lint/suspicious/noExplicitAny
 export function parseWord(word: any): Word | null {
-	const meanings = word.meanings;
+  const meanings = word.meanings;
 
-	if (!meanings || meanings.length === 0) {
-		return null;
-	}
+  if (!meanings || meanings.length === 0) {
+    return null;
+  }
 
-	const phonestics = word.phonetics;
+  const phonetics = word.phonetics;
 
-	if (!phonestics || phonestics.length === 0) {
-		return null;
-	}
+  if (!phonetics || phonetics.length === 0) {
+    return null;
+  }
 
-	return {
-		word: word.word,
-		pronounciation: word.phonetic ?? findPhonetic(word.phonetics),
-		definition: meanings[0].definitions[0].definition,
-		synonyms: meanings[0].synonyms,
-		antonyms: meanings[0].antonyms,
-		sentences: extractExamples(meanings),
-	};
+  return {
+    word: word.word,
+    phonetics: word.phonetic ?? findPhonetics(word.phonetics),
+    definition: meanings[0].definitions[0].definition,
+    synonyms: meanings[0].synonyms,
+    antonyms: meanings[0].antonyms,
+    sentences: extractExamples(meanings),
+  };
 }
 
 // biome-ignore lint: lint/suspicious/noExplicitAny
-function findPhonetic(phonoetics: any[]): string {
-	if (phonoetics.length === 0) return "";
+function findPhonetics(phonoetics: any[]): string {
+  if (phonoetics.length === 0) return "";
 
-	let result = "";
-	for (const ph of phonoetics) {
-		if (ph.text) {
-			result = ph.text;
-			break;
-		}
-	}
+  let result = "";
+  for (const ph of phonoetics) {
+    if (ph.text) {
+      result = ph.text;
+      break;
+    }
+  }
 
-	return result;
+  return result;
 }
 
 // biome-ignore lint: lint/suspicious/noExplicitAny
 function extractExamples(meanings: any[]): string[] {
-	const examples: string[] = [];
-	for (const meaning of meanings) {
-		for (const definition of meaning.definitions) {
-			if (definition.example) {
-				examples.push(definition.example);
-			}
-		}
-	}
-	return examples;
+  const examples: string[] = [];
+  for (const meaning of meanings) {
+    for (const definition of meaning.definitions) {
+      if (definition.example) {
+        examples.push(definition.example);
+      }
+    }
+  }
+  return examples;
 }
